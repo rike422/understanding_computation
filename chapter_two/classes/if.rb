5b1edb915939f2efc67a9ec1d2de334c0a475efb
rike422 <rike2Takira@gmail.com>
2015-02-17 00:35:41 +0900

    add if.rb

---
+++ chapter_two/classes/if.rb
@@ -0,0 +1,21 @@
+class If < Struct.new(:condition, :consequence, :alternative)
+  def to_s
+    "If (#{condition}) { #{consequence} } else { #{alternative} }"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce(env)
+    if condition.reducible?
+      [If.new(condition.reduce(env), consequence, alternative), env]
+    else
+      case condition
+      when Boolean.new(true)
+        [consequence, env]
+      when Boolean.new(true)
+        [env, consequence]
+      end
+    end
+  end
